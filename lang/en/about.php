4527f8965cd9e04de42a367e9381385824bb014c
BenyaminRmb <benyaminrmb@gmail.com>
2024-04-18 23:39:37 +0330

    (Feat): write down some summary.

--- lang/en/about.php
+++ lang/en/about.php
@@ -2,5 +2,5 @@
 return [
     'greeting'=>"Hi, I'm Benyamin Bolhassani",
     'this_is_ben'=>"This Is Benyamin",
-    'tiny_about'=>"Hello! I'm Benyamin Bolhassani, a programmer specializing in <a class='link' href='https://nuxt.com'>Nuxt</a> and <a class='link' href='https://laravel.com'>Laravel</a>. Currently, I live in Tehran and have 5.5 years of experience in this field. I'm passionate about learning and sharing knowledge, striving for both my professional development and others'. Feel free to contact me for collaboration or further information.",
+    'tiny_about'=>"A programmer specializing in <a class='link' href='https://nuxt.com'>Nuxt</a> and <a class='link' href='https://laravel.com'>Laravel</a>. Currently, I live in Tehran and have 5.5 years of experience in this field. I'm passionate about learning and sharing knowledge, striving for both my professional development and others'. Feel free to contact me for collaboration or further information.",
 ];
